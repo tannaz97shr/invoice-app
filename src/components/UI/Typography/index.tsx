26f07e6f37c7aac46e96c45fb3d0af6ab5d11e31
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-20 19:10:28 +0300

    feat: typography

---
+++ src/components/UI/Typography/index.tsx
@@ -0,0 +1,43 @@
+export const HeadingLarg = ({ children }: { children: React.ReactNode }) => (
+  <h1 className=" font-bold text-4xl leading-[33px] tracking-[-1px]">
+    {children}
+  </h1>
+);
+
+export const HeadingMedium = ({ children }: { children: React.ReactNode }) => (
+  <h2 className=" font-bold text-2xl leading-[22px] tracking-[-0.75px]">
+    {children}
+  </h2>
+);
+
+export const HeadingSmall = ({ children }: { children: React.ReactNode }) => (
+  <h3 className=" font-bold text-[15px] leading-[24px] tracking-[-0.25px]">
+    {children}
+  </h3>
+);
+
+export const HeadingSmallVariant = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <h4 className=" font-bold text-[15px] leading-[15px] tracking-[-0.25px]">
+    {children}
+  </h4>
+);
+
+export const Paragraph = ({ children }: { children: React.ReactNode }) => (
+  <p className=" font-bold text-sm leading-[18px] tracking-[-0.1px]">
+    {children}
+  </p>
+);
+
+export const ParagraphVariant = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => (
+  <p className=" font-bold text-sm leading-[15px] tracking-[-0.25px]">
+    {children}
+  </p>
+);
