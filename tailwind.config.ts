cdd6ffae782377cd79cb077cc1308a7c3d1060f2
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-20 13:16:43 +0300

    feat: set colors

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,tsx}"],
+  theme: {
+    colors: {
+      "blue-cornflower": "#7C5DFA",
+      heliotrope: "#9277FF",
+      pink: "#ff49db",
+      mirage: "#1E2139",
+      "ebony-clay": "#252945",
+      selago: "#DFE3FA",
+      "bali-hai": "#888EB0",
+      "ship-cove": "#7E88C3",
+      vulcan: "#0C0E16",
+      "burnt-sienna": "#EC5757",
+      "mona-lisa": "#FF9797",
+      "bg-light": "#F8F8FB",
+      "mirage-dark": "#141625",
+    },
+  },
+  plugins: [],
+};
