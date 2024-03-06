467a5d4e72871449da7a082f99b50a015248c4ac
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-03-06 16:40:50 +0300

    feat: dark mode input

--- src/components/UI/TextInput/index.tsx
+++ src/components/UI/TextInput/index.tsx
@@ -6,13 +6,17 @@ interface TextInputProps {
 const TextInput = ({ label, name }: TextInputProps) => {
   return (
     <label className=" flex flex-col">
-      <span className=" capitalize text-ship-cove font-medium text-sm">
+      <span
+        className=" capitalize text-ship-cove font-medium text-sm
+      dark:text-selago"
+      >
         {label}
       </span>
       <input
         name={name}
         className="border border-selago py-4 px-5 text-base rounded mt-2 font-bold text-vulcan 
-        focus-visible:outline-offset-1 focus-visible:outline-heliotrope"
+        focus-visible:outline-offset-1 focus-visible:outline-heliotrope
+        dark:bg-mirage dark:text-white dark:focus-visible:outline-none dark:border-ebony-clay"
       />
     </label>
   );
