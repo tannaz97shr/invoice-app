939d7b0d6bcae5d2ae8324060c03f6ff64da7cd3
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-03-11 22:03:16 +0300

    feat: dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/UI/Button";
+import Dropdown from "./components/UI/Dropdown";
 import TextInput from "./components/UI/TextInput";
 import ThemeSwitch from "./components/UI/ThemeSwitch";
 
@@ -8,6 +9,34 @@ function App() {
       <ThemeSwitch />
       <Button variant="long">Mark as Paid</Button>
       <TextInput label="street address" name="street" />
+      <div className=" w-80">
+        <Dropdown
+          className="ml-6"
+          label="Payment Terms"
+          initialValue={{
+            option: "Net 1 Day",
+            value: "1",
+          }}
+          options={[
+            {
+              option: "Net 1 Day",
+              value: "1",
+            },
+            {
+              option: "Net 7 Days",
+              value: "7",
+            },
+            {
+              option: "Net 14 Days",
+              value: "14",
+            },
+            {
+              option: "Net 30 Days",
+              value: "30",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/UI/Dropdown/index.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { IDropdownOption } from "../../../models/general";
+import { IconArrowDown } from "../Icons";
+
+interface DropdownProps {
+  options: IDropdownOption[];
+  initialValue: IDropdownOption;
+  label: string;
+  className?: string;
+}
+
+const Dropdown = ({
+  options,
+  initialValue,
+  label,
+  className,
+}: DropdownProps) => {
+  const [isActive, setIsActive] = useState(false);
+  const [selected, setIsSelected] = useState(initialValue);
+  console.log(selected);
+  return (
+    <div className={`flex flex-col relative ${className && className}`}>
+      <span
+        className="capitalize text-ship-cove font-medium text-sm
+dark:text-selago"
+      >
+        {label}
+      </span>
+      <button
+        onClick={(e) => {
+          setIsActive(!isActive);
+        }}
+        className=" w-full flex items-center justify-between border border-selago py-4 px-5 text-base rounded mt-2 font-bold text-vulcan 
+        focus-visible:outline-offset-1 focus-visible:outline-heliotrope
+        dark:bg-mirage dark:text-white dark:focus-visible:outline-none dark:border-ebony-clay
+          focus:border-blue-cornflower"
+      >
+        <span>{selected.option}</span>
+        <IconArrowDown />
+      </button>
+      {isActive && (
+        <ul className=" absolute shadow-lg bg-white w-full top-[120%] rounded-lg">
+          {options.map((option, index) => (
+            <li
+              className={`px-6 py-4 ${
+                index !== options.length && "border-b border-selago"
+              } text-base mt-2 font-bold text-vulcan cursor-pointer hover:text-blue-cornflower`}
+              key={option.value}
+              onClick={() => {
+                setIsSelected(option);
+                setIsActive(false);
+              }}
+            >
+              {option.option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
