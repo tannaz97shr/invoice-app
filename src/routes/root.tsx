316fd98b5667996a6348f0594f8b018fc9452197
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-03-23 12:35:08 +0300

    feat: main menu desktop view

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,7 +3,7 @@ import ThemeSwitch from "../components/UI/ThemeSwitch";
 
 export default function Root() {
   return (
-    <div className=" flex flex-col">
+    <div className=" flex flex-col md:flex-row">
       <MainMenu />
       <div>
         content
