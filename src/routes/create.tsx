266a390044635b19fedd3b0dc262d879a49b4afa
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-04-08 10:30:01 +0300

    feat: create form desktop view

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -19,9 +19,18 @@ export default function Create() {
       <div className=" text-blue-cornflower font-bold mb-6">Bill Form</div>
       <Form method="post">
         <TextInput label="Street Address" name="street" />
-        <div className="flex gap-2 mt-4">
-          <TextInput className="w-1/2" label="City" name="city" />
-          <TextInput className="w-1/2" label="Post Code" name="postCode" />
+        <div className="flex flex-wrap mt-4 justify-between">
+          <TextInput className="w-[48%] md:w-[31%]" label="City" name="city" />
+          <TextInput
+            className="w-[48%] md:w-[31%]"
+            label="Post Code"
+            name="postCode"
+          />
+          <TextInput
+            className="w-full mt-4 md:w-[31%] md:mt-0"
+            label="Country"
+            name="country"
+          />
         </div>
         <div className=" text-blue-cornflower font-bold my-6">Bill To</div>
         <TextInput label="Client's Name" name="clientName" />
@@ -31,42 +40,52 @@ export default function Create() {
           label="Street Address"
           name="clientStreet"
         />
-        <div className="flex gap-2 mt-4">
-          <TextInput className="w-1/2" label="City" name="clientCity" />
+        <div className="flex flex-wrap mt-4 justify-between">
           <TextInput
-            className="w-1/2"
+            className="w-[48%] md:w-[31%]"
+            label="City"
+            name="clientCity"
+          />
+          <TextInput
+            className="w-[48%] md:w-[31%]"
             label="Post Code"
             name="ClientPostCode"
           />
+          <TextInput
+            className="w-full mt-4 md:w-[31%] md:mt-0"
+            label="Country"
+            name="clientCountry"
+          />
         </div>
-        <TextInput className="mt-4" label="Country" name="clientCountry" />
-        <DatePicker className="mt-4" label="Invoice Date" />
-        <Dropdown
-          label="Payment Terms"
-          className="mt-4"
-          options={[
-            {
+        <div className=" flex flex-col md:flex-row md:gap-2 md:mt-4 md:items-center">
+          <DatePicker className="mt-4 md:mt-0 md:flex-1" label="Invoice Date" />
+          <Dropdown
+            label="Payment Terms"
+            className="mt-4 md:mt-0 md:flex-1"
+            options={[
+              {
+                option: "Net 1 Day",
+                value: "1",
+              },
+              {
+                option: "Net 7 Days",
+                value: "7",
+              },
+              {
+                option: "Net 14 Days",
+                value: "14",
+              },
+              {
+                option: "Net 30 Days",
+                value: "30",
+              },
+            ]}
+            initialValue={{
               option: "Net 1 Day",
               value: "1",
-            },
-            {
-              option: "Net 7 Days",
-              value: "7",
-            },
-            {
-              option: "Net 14 Days",
-              value: "14",
-            },
-            {
-              option: "Net 30 Days",
-              value: "30",
-            },
-          ]}
-          initialValue={{
-            option: "Net 1 Day",
-            value: "1",
-          }}
-        />
+            }}
+          />
+        </div>
         <TextInput
           className="mt-4"
           label="Project Description"
